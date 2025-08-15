186e8e995ce63da6b886460942fb925c5051c91a
Fernando Sávio Gomes <133050259+fernandosaviogo@users.noreply.github.com>
2025-08-14 21:44:57 -0300

    Sequelize configuration

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,8 +1,16 @@
+import Aluno from '../models/Aluno'
+
 class HomeController {
-  index(req, res) {
-    res.json({
-      tudoCerto: true,
+  async index(req, res) {
+    const novoAluno = await Aluno.create({
+      name: 'Maria',
+      sobrenome: 'Joaquina',
+      email: 'maria@gmail.com',
+      idade: 25,
+      peso: 65,
+      altura: 1.7,
     });
+    res.json({novoAluno});
   }
 }
 
